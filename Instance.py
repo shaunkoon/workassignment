8f7b6e90494249a76573ef5448afff87e2b09a7a
Shaun Koon <SKoon@spartaglobal.com>
2019-05-28 23:30:59 +0100

    first commit

---
+++ Instance.py
@@ -0,0 +1,26 @@
+instance = 'An example or single occurrence of something'
+variable = 'Variables are reserved memory locations to store values'
+string = 'A list of characters in order.'
+integer = 'A whole number'
+
+# how to assign and re assign variable
+# y = 5
+# print(y)
+# y = 'y not'
+# print(y)
+
+
+print('Instance')
+print(instance)
+
+print('Variable')
+print(variable)
+
+print('String')
+print(string)
+print(type(string))
+
+x = 5
+print('integer')
+print(integer)
+print(type(x))
